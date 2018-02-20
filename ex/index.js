05786bde0178fd5578e22c7eb59e97fc894cfce6
Renato Martins <renatto.martins@gmail.com>
2018-02-20 07:43:08 -0300

    Start to test ECMAscript 6 classes in index.js

--- ex/index.js
+++ ex/index.js
@@ -1,1 +1,12 @@
-require('./duvidaCruel')
+class Pessoa {
+    constructor(nome) {
+        this.nome = nome
+    }
+
+    toString() {
+        return `Pessoa: ${this.nome}`
+    }
+}
+
+const pessoa = new Pessoa('Renato Martins')
+console.log(pessoa.toString())
